f83591ab8054fcb3f3f120fc5817d83431123ee4
geogeorgiou <retali007>
2021-12-20 00:44:50 +0200

    Providing Profile page and dummy PostFeed page

--- pages/[username]/index.tsx
+++ pages/[username]/index.tsx
@@ -1,7 +1,62 @@
-/**
- * No issue with enter and admin routes
- * cuz next will give priority to static routes instead of dynamic like [username]
- */
-export default function UserProfile({}) {
-	return <main></main>;
+import { getUserWithUsername, postToJSON, firestore } from "@lib/firebase";
+import { query, collection, where, getDocs, limit, orderBy, getFirestore } from "firebase/firestore";
+import UserProfile from "@components/UserProfile";
+import { GetStaticProps, GetStaticPaths, GetServerSideProps } from "next";
+import PostFeed from "@components/PostFeed";
+import { User } from "@firebase/auth";
+
+// interface UsernameIndexSSRProps {
+// 	query: queryParam;
+// }
+
+export type UserType = {
+	user: User | null;
+};
+
+export async function getServerSideProps({ query: urlQuery }: any) {
+	const { username } = urlQuery;
+
+	const userDoc = await getUserWithUsername(username);
+
+	// If no user, short circuit to 404 page
+	// if (!userDoc) {
+	// 	return {
+	// 		notFound: true,
+	// 	};
+	// }
+
+	// JSON serializable data
+	let user = null;
+	let posts = null;
+
+	if (userDoc) {
+		user = userDoc.data();
+		// const postsQuery = userDoc.ref
+		//   .collection('posts')
+		//   .where('published', '==', true)
+		//   .orderBy('createdAt', 'desc')
+		//   .limit(5);
+
+		const postsQuery = query(
+			collection(getFirestore(), userDoc.ref.path, "posts"),
+			where("published", "==", true),
+			orderBy("createdAt", "desc"),
+			limit(5)
+		);
+		posts = (await getDocs(postsQuery)).docs.map(postToJSON);
+	}
+
+	return {
+		props: { user, posts }, // will be passed to the page component as props
+	};
+}
+
+export default function UserProfilePage({ user, posts }: any) {
+	return (
+		<main>
+			{/* <Metatags title={user.username} description={`${user.username}'s public profile`} /> */}
+			<UserProfile user={user} />
+			<PostFeed posts={posts} admin={false} />
+		</main>
+	);
 }
